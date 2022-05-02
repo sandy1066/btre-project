ce26aed8196958d730e36237c5538e8394b61884
Sandeep Kumar <techbrain.sk.1996@gmail.com>
2022-05-03 01:41:54 +0530

    Revert "verification for PR"
    This reverts commit ae44cb9f7a82b56d1a3a168ea910cc0cbd53c41a.

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h2>Hello World</h2>')
+    return HttpResponse('<h1>Hello World</h1>')
